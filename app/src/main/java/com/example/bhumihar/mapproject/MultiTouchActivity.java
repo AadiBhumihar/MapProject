b980c183de0d696674284718a70151f5cd5f7079
aadibhumihar <201451032@iiitvadodara.ac.in>
2017-06-14 09:26:15 +0530

    Commit

---
+++ app/src/main/java/com/example/bhumihar/mapproject/MultiTouchActivity.java
@@ -0,0 +1,24 @@
+package com.example.bhumihar.mapproject;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MultiTouchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_touch);
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/bhumihar/mapproject/MultitouchView.java
@@ -0,0 +1,128 @@
+package com.example.bhumihar.mapproject;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.AttributeSet;
+import android.util.SparseArray;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by bhumihar on 13/6/17.
+ */
+
+public class MultitouchView extends View {
+
+    private static final int SIZE = 60;
+
+    private SparseArray<PointF> mActivePointers;
+    private Paint mPaint;
+    private int[] colors = {Color.BLUE, Color.GREEN, Color.MAGENTA,
+            Color.BLACK, Color.CYAN, Color.GRAY, Color.RED, Color.DKGRAY,
+            Color.LTGRAY, Color.YELLOW};
+
+    private Paint textPaint;
+
+    public MultitouchView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    private void initView() {
+        mActivePointers = new SparseArray<PointF>();
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        // set painter color to a color you like
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setTextSize(20);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // get pointer index from the event object
+        int pointerIndex = event.getActionIndex();
+
+        // get pointer ID
+        int pointerId = event.getPointerId(pointerIndex);
+
+        // get masked (not specific to a pointer) action
+        int maskedAction = event.getActionMasked();
+
+        switch (maskedAction) {
+
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_POINTER_DOWN: {
+                // We have a new pointer. Lets add it to the list of pointers
+
+                PointF f = new PointF();
+                f.x = event.getX(pointerIndex);
+                f.y = event.getY(pointerIndex);
+                mActivePointers.put(pointerId, f);
+                int size = event.getPointerCount();
+                if (size==3){
+                    call();
+                }else {
+                    Toast.makeText(getContext(), "Please Put Three fingers", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: { // a pointer was moved
+                for (int size = event.getPointerCount(), i = 0; i < size; i++) {
+                    PointF point = mActivePointers.get(event.getPointerId(i));
+                    if (point != null) {
+                        point.x = event.getX(i);
+                        point.y = event.getY(i);
+                    }
+                }
+                break;
+            }
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+            case MotionEvent.ACTION_CANCEL: {
+                mActivePointers.remove(pointerId);
+                break;
+            }
+        }
+        invalidate();
+
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // draw all pointers
+        for (int size = mActivePointers.size(), i = 0; i < size; i++) {
+            PointF point = mActivePointers.valueAt(i);
+            if (point != null)
+                mPaint.setColor(colors[i % 9]);
+            canvas.drawCircle(point.x, point.y, SIZE, mPaint);
+        }
+        canvas.drawText("Total pointers: " + mActivePointers.size(), 10, 40, textPaint);
+    }
+
+    private void call() {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:9472967420"));
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            ((Activity) getContext()).startActivity(callIntent);
+        }
+
+    }
+
+
+}
